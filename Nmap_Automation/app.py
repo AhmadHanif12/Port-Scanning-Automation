cdfe43d0c92c5f6e01624939c214239f4cb780e7
arslanjv <arslanjaved1905@gmail.com>
2024-07-18 16:29:15 +0500

    Final Commit
    debugger mode off.

--- Nmap_Automation/app.py
+++ Nmap_Automation/app.py
@@ -147,4 +147,4 @@ def script_results():
         return render_template('script_results.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
